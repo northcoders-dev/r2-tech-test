eca465226b5444398e78ba8ccaed5da1406030f3
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2022-02-11 10:44:29 +0000

    add insert data functionality to seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,14 +1,37 @@
 const db = require("../index.js");
 
 const seed = (data) => {
-  return db.query(`DROP TABLE IF EXISTS recipes`).then(() => {
-    return db.query(
-      `CREATE TABLE recipes (
+  return db
+    .query(`DROP TABLE IF EXISTS recipes`)
+    .then(() => {
+      return db.query(
+        `CREATE TABLE recipes (
             id VARCHAR(15) UNIQUE PRIMARY KEY NOT NULL
             imageUrl VARCHAR(255) NOT NULL,
             instructions VARCHAR(255) NOT NULL,
-            ingredients VARCHAR(100)[] NOT NULL,CURRENT_TIMESTAMP
+            ingredients VARCHAR(100)[][] NOT NULL
         );`
-    );
-  });
+      );
+    })
+    .then(() => {
+      return db.query(
+        format(
+          `INSERT INTO recipes(id, imageUrl, instructions, ingredients) VALUES %L RETURNING *;`
+        ),
+        data.map((recipe) => {
+          const ingredientsArrayString = "ARRAY[";
+          recipe.ingredients.forEach((ingredient) => {
+            ingredientsArrayString += `[${ingredient.name}, '${ingredient.grams}']`;
+          });
+          ingredientsArrayString += `]`;
+
+          return [
+            recipe.id,
+            recipe.imageUrl,
+            recipe.instructions,
+            ingredientsArrayString,
+          ];
+        })
+      );
+    });
 };
