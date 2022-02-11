94c7e68ec773c00243608e8c3fc07e472ce4eec5
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2022-02-11 10:04:02 +0000

    add create table functionality to seed.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,14 @@
 const db = require("../index.js");
 
 const seed = (data) => {
-  return db.query(`DROP TABLE IF EXISTS recipes`);
+  return db.query(`DROP TABLE IF EXISTS recipes`).then(() => {
+    return db.query(
+      `CREATE TABLE recipes (
+            id VARCHAR(15) UNIQUE PRIMARY KEY NOT NULL
+            imageUrl VARCHAR(255) NOT NULL,
+            instructions VARCHAR(255) NOT NULL,
+            ingredients VARCHAR(100)[] NOT NULL,CURRENT_TIMESTAMP
+        );`
+    );
+  });
 };
